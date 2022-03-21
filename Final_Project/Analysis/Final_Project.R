92aba32d4a08822798d03fe31d67fded6dd19b28
Connor <97637205+ccabes15@users.noreply.github.com>
2022-03-21 19:47:36 -0400

    Update Final_Project.R

--- Final_Project/Analysis/Final_Project.R
+++ Final_Project/Analysis/Final_Project.R
@@ -7,10 +7,23 @@ abalone_data <- read_csv(file = "../Data/abalone_data.csv", col_names =
                          c("Sex", "Length", "Diameter", "Height", "Whole_Weight", "Shucked_Weight",
                            "Viscera_Weight", "Shell_Weight", "Rings"))
 
-ggpairs(data = abalone_data)
+#There are two outliers that are more than 3 SDs outside the mean within the Height Variable
+# By using Height < 0.3, we are able to remove these two outliers and get much better scatter-
+#plots
 
-multmod_abalone <- lm(Rings ~ Sex + Length + Diameter + Height + Whole_Weight + Shucked_Weight + Viscera_Weight +
+sd(abalone_data$Height)
+mean(abalone_data$Height)
+
+abalone_data %>% 
+  filter(Height < 0.3) %>% 
+  ggpairs()
+
+multmod_abalone <- lm(Rings ~ Length + Diameter + Height + Whole_Weight + Shucked_Weight + Viscera_Weight +
                       Shell_Weight, data = abalone_data)
 
 mult_abalone <- tidy(multmod_abalone)
 mult_abalone
+
+sd(abalone_data$Height)
+mean(abalone_data$Height)
+
