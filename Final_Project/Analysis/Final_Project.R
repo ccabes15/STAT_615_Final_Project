74b94c97e77776e8ddfcb59de32c3432384cf31b
Connor <97637205+ccabes15@users.noreply.github.com>
2022-03-21 22:22:09 -0400

    Update Final_Project.R

--- Final_Project/Analysis/Final_Project.R
+++ Final_Project/Analysis/Final_Project.R
@@ -14,11 +14,16 @@ abalone_data <- read_csv(file = "../Data/abalone_data.csv", col_names =
 sd(abalone_data$Height)
 mean(abalone_data$Height)
 
+#Based off a test statistic of -0.861 and an associated p-value of .389, we should eliminate length 
+#as a predictor variable within the multivariate regression model
+
+abalone_data$Sex <- recode(abalone_data$Sex, "M" = 1, "F" = 2, "I" = 3)
+
 abalone_data %>% 
   filter(Height < 0.3) %>% 
   ggpairs()
 
-multmod_abalone <- lm(Rings ~ Length + Diameter + Height + Whole_Weight + Shucked_Weight + Viscera_Weight +
+multmod_abalone <- lm(Rings ~ Sex + Diameter + Height + Whole_Weight + Shucked_Weight + Viscera_Weight +
                       Shell_Weight, data = abalone_data)
 
 mult_abalone <- tidy(multmod_abalone)
